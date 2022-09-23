6fb99928826e1247366e039240f5fb371e550b1f
DarrelA <darrelaiscoding@gmail.com>
2022-09-23 09:42:01 +0800

    Type check for login action

--- client/src/actions/userAction.tsx
+++ client/src/actions/userAction.tsx
@@ -1,7 +1,15 @@
+import { AnyAction } from 'redux';
+import { ThunkAction, ThunkDispatch } from 'redux-thunk';
 import { USER_LOGIN_SUCCESS } from '../constants/userConstants';
+import { RootState } from '../store';
 
-export const login = (email: string, password: string) => async (dispatch) => {
-  try {
-    dispatch({ type: USER_LOGIN_SUCCESS });
-  } catch (e) {}
-};
+export const login =
+  (
+    email: string,
+    password: string
+  ): ThunkAction<Promise<void>, RootState, unknown, AnyAction> =>
+  async (dispatch: ThunkDispatch<RootState, unknown, AnyAction>): Promise<void> => {
+    try {
+      dispatch({ type: USER_LOGIN_SUCCESS });
+    } catch (e) {}
+  };
